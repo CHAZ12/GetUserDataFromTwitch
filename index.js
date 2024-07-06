c64d064f51de8088e6cb82e392c4a33a4cef3423
KillerRain <zpmass@gmail.com>
2024-07-05 23:13:42 -0500

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios')
-const express = require('express');
-const udon = require('./api/udon');
-const app = express();
-
-app.use("/api/udon", udon);
-// PORT  Set in windows with set PORT = #
-const port = process.env.PORT || 9001;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
